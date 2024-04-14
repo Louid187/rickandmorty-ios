ec0e30baed5d47145765677fdff4508b4d2b26ed
Louis <louisstephan@Louiss-Air.fritz.box>
2024-04-14 18:56:40 +0200

    Added Documentations and API Files

---
+++ RickAndMorty/APIClients/RMRequest.swift
@@ -0,0 +1,8 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Louis on 14.04.24.
+//
+
+import Foundation
---
+++ RickAndMorty/APIClients/RMService.swift
@@ -0,0 +1,8 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Louis on 14.04.24.
+//
+
+import Foundation
